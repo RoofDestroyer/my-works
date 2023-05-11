7159387e36695f9e81cbff8b5892275534864392
Nikita <1nikita7@mail.ru>
2023-05-11 17:31:09 +0300

    Реализовал кликер

---
+++ test/src/components/test-count.jsx
@@ -0,0 +1,14 @@
+const { useState } = require("react");
+
+const TestCount = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Клик</button>
+    </div>
+  );
+};
+
+export default TestCount;
